0b96148a77ec17b649c187fdec891fee92b6f94f
David Sherret <dsherret@gmail.com>
2017-03-22 22:29:14 -0400

    Add TypeParameteredNode to Class, Interface, and Type Alias (in addition to SignatureDeclaration)

--- src/compiler/class/ClassDeclaration.ts
+++ src/compiler/class/ClassDeclaration.ts
@@ -1,9 +1,9 @@
 ﻿import * as ts from "typescript";
 import {Node} from "./../common";
-import {NamedNode, ExportableNode, AmbientableNode, DocumentationableNode} from "./../base";
+import {NamedNode, ExportableNode, AmbientableNode, DocumentationableNode, TypeParameteredNode} from "./../base";
 import {MethodDeclaration} from "./MethodDeclaration";
 
-export const ClassDeclarationBase = DocumentationableNode(AmbientableNode(ExportableNode(NamedNode(Node))));
+export const ClassDeclarationBase = TypeParameteredNode(DocumentationableNode(AmbientableNode(ExportableNode(NamedNode(Node)))));
 export class ClassDeclaration extends ClassDeclarationBase<ts.ClassDeclaration> {
     /**
      * Gets the class method declarations.
--- src/compiler/interface/InterfaceDeclaration.ts
+++ src/compiler/interface/InterfaceDeclaration.ts
@@ -1,7 +1,7 @@
 ﻿import * as ts from "typescript";
 import {Node} from "./../common";
-import {NamedNode, ExportableNode, AmbientableNode, DocumentationableNode} from "./../base";
+import {NamedNode, ExportableNode, AmbientableNode, DocumentationableNode, TypeParameteredNode} from "./../base";
 
-export const InterfaceDeclarationBase = DocumentationableNode(AmbientableNode(ExportableNode(NamedNode(Node))));
+export const InterfaceDeclarationBase = TypeParameteredNode(DocumentationableNode(AmbientableNode(ExportableNode(NamedNode(Node)))));
 export class InterfaceDeclaration extends InterfaceDeclarationBase<ts.InterfaceDeclaration> {
 }
