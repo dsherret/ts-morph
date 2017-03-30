ab2a5430444c6fed7d60ca394362f1bf3c20d83e
David Sherret <dsherret@gmail.com>
2017-03-30 14:41:45 -0400

    Add addModifier to ModifierableNode.

--- src/compiler/base/ModifierableNode.ts
+++ src/compiler/base/ModifierableNode.ts
@@ -7,6 +7,7 @@ export interface ModifierableNode {
     getModifiers(): Node<ts.Node>[];
     getCombinedModifierFlags(): ts.ModifierFlags;
     getFirstModifierByKind(kind: ts.SyntaxKind): Node<ts.Node> | undefined;
+    addModifier(text: string, addAfterModifierTexts: string[]): Node<ts.Node>;
 }
 
 export function ModifierableNode<T extends Constructor<ModiferableNodeExtensionType>>(Base: T): Constructor<ModifierableNode> & T {
@@ -36,6 +37,46 @@ export function ModifierableNode<T extends Constructor<ModiferableNodeExtensionT
             }
 
             return undefined;
+        }
+
+        /**
+         * Add a modifier with the specified text.
+         * @param text - Modifier text to add.
+         * @param addAfterModifierTexts - Add after these modifier texts.
+         * @returns The added modifier.
+         * @internal
+         */
+        addModifier(text: string, addAfterModifierTexts: string[]): Node<ts.Node> {
+            // get insert position
+            const modifiers = (this.node.modifiers || []) as ts.NodeArray<ts.Modifier>;
+            let insertPos = this.node.pos;
+            addAfterModifierTexts.forEach(addAfterText => {
+                for (let modifier of modifiers) {
+                    if (modifier.getText() === addAfterText) {
+                        if (insertPos < modifier.end)
+                            insertPos = modifier.end;
+                        break;
+                    }
+                }
+            });
+
+            // insert setup
+            let startPos: number;
+            const isFirstModifier = insertPos === this.node.pos;
+            if (isFirstModifier) {
+                text = text + " ";
+                startPos = insertPos;
+            }
+            else {
+                text = " " + text;
+                startPos = insertPos + 1;
+            }
+
+            // insert
+            const sourceFile = this.getRequiredSourceFile();
+            sourceFile.insertText(insertPos, text);
+
+            return this.getModifiers().filter(m => m.getStart() === startPos)[0];
         }
     };
 }
--- src/tests/compiler/base/modifierableNodeTests.ts
+++ src/tests/compiler/base/modifierableNodeTests.ts
@@ -35,4 +35,21 @@ describe(nameof(ModifierableNode), () => {
             expect(modifiers[1].getKind()).to.equal(ts.SyntaxKind.AbstractKeyword);
         });
     });
+
+    describe(nameof<ModifierableNode>(n => n.addModifier), () => {
+        it("should add a modifier at the start when not specifying any order", () => {
+            const {firstChild} = getInfoFromText<ClassDeclaration>("class Identifier {}");
+            firstChild.addModifier("abstract", []);
+            firstChild.addModifier("export", []);
+            expect(firstChild.getText()).to.equal("export abstract class Identifier {}");
+        });
+
+        it("should add a modifier after a text when specifying it", () => {
+            const {firstChild} = getInfoFromText<ClassDeclaration>("class Identifier {}");
+            firstChild.addModifier("export", []);
+            firstChild.addModifier("abstract", ["export", "declare"]);
+            firstChild.addModifier("declare", ["export"]);
+            expect(firstChild.getText()).to.equal("export declare abstract class Identifier {}");
+        });
+    });
 });
