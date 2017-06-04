80342c94d1ead0f7be1b7c2e3f57be52af7a43b3
David Sherret <dsherret@gmail.com>
2017-06-03 22:34:43 -0400

    Improve checking for blank line.
    Iterate over string instead of using regex with a substring for performance reasons.

--- src/manipulation/isBlankLineAtPos.ts
+++ src/manipulation/isBlankLineAtPos.ts
@@ -2,5 +2,25 @@
 
 export function isBlankLineAtPos(sourceFile: SourceFile, pos: number) {
     const fullText = sourceFile.getFullText(sourceFile);
-    return /^\s*(\r?\n)\s*(\r?\n)/.test(fullText.substring(pos));
+    let foundBlankLine = false;
+
+    for (let i = pos; i < fullText.length; i++) {
+        const char = fullText[i];
+
+        if (char === " " || char === "\t")
+            continue;
+        if (char === "\r" && fullText[i + 1] === "\n" || char === "\n") {
+            if (foundBlankLine)
+                return true;
+            foundBlankLine = true;
+
+            if (char === "\r")
+                i++;
+            continue;
+        }
+
+        return false;
+    }
+
+    return false;
 }
