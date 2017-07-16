d2ccbd374d4efb20eac3dc833982806aa50b6928
David Sherret <dsherret@gmail.com>
2017-07-16 13:20:22 -0400

    Add jsdocs to AdvancedIterator.

--- src/utils/AdvancedIterator.ts
+++ src/utils/AdvancedIterator.ts
@@ -1,5 +1,8 @@
 ﻿import * as errors from "./../errors";
 
+/**
+ * Advanced IterableIterator.
+ */
 export class AdvancedIterator<T> {
     private readonly buffer: (T | undefined)[] = Array.from(Array(3)); // previous, current, next
     private bufferIndex = 0;
@@ -10,10 +13,12 @@ export class AdvancedIterator<T> {
         this.advance();
     }
 
+    /** If the iterator has reached the end. */
     get done() {
         return this.isDone;
     }
 
+    /** Current value. */
     get current() {
         if (this.nextCount === 0)
             throw new errors.InvalidOperationError("Cannot get the current when the iterator has not been advanced.");
@@ -22,12 +27,14 @@ export class AdvancedIterator<T> {
         return this.buffer[this.bufferIndex];
     }
 
+    /** Previous value. */
     get previous() {
         if (this.nextCount <= 1)
             throw new errors.InvalidOperationError("Cannot get the previous when the iterator has not advanced enough.");
         return this.buffer[(this.bufferIndex + this.buffer.length - 1) % this.buffer.length];
     }
 
+    /** Iterates forward and returns the next value. */
     next() {
         if (this.done)
             throw new errors.InvalidOperationError("Cannot get the next when at the end of the iterator.");
@@ -38,6 +45,7 @@ export class AdvancedIterator<T> {
         return next;
     }
 
+    /** Iterates over the rest of the values */
     *rest() {
         while (!this.done)
             yield this.next();
