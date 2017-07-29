6e45898cdac499ccb30692468ff9c3a6bbaa5332
David Sherret <dsherret@gmail.com>
2017-07-29 13:17:50 -0400

    Fix definition file error.

--- src/compiler/callBaseFill.ts
+++ src/compiler/callBaseFill.ts
@@ -2,6 +2,7 @@
 
 // todo: add code verification to ensure all fill functions call this
 
+/** @internal */
 export function callBaseFill(basePrototype: any, node: Node, structure: any) {
     if (basePrototype.fill != null)
         basePrototype.fill.call(node, structure);
--- src/compiler/callBaseGetStructure.ts
+++ src/compiler/callBaseGetStructure.ts
@@ -2,6 +2,7 @@
 
 // todo: add code verification to ensure all fill functions call this
 
+/** @internal */
 export function callBaseGetStructure<T>(basePrototype: any, node: Node, structure: MakeRequired<T>): any {
     let newStructure: T;
     if (basePrototype.getStructure != null)
