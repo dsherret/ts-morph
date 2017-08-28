58a7ba84eb8e8dce5c309da628df7afaff9192b8
David Sherret <dsherret@gmail.com>
2017-08-28 19:51:59 -0400

    #63 - Add ArrayLiteralExpression

---
+++ src/compiler/common/ArrayLiteralExpression.ts
@@ -0,0 +1,11 @@
+﻿import * as ts from "typescript";
+import {Expression} from "./Expression";
+
+export class ArrayLiteralExpression extends Expression<ts.ArrayLiteralExpression> {
+    /**
+     * Gets the array's elements.
+     */
+    getElements(): Expression[] {
+        return this.compilerNode.elements.map(e => this.global.compilerFactory.getNodeFromCompilerNode(e, this.sourceFile)) as Expression[];
+    }
+}
---
+++ src/tests/compiler/common/arrayLiteralExpressionTests.ts
@@ -0,0 +1,30 @@
+﻿import * as ts from "typescript";
+import {expect} from "chai";
+import {ArrayLiteralExpression, VariableStatement} from "./../../../compiler";
+import {getInfoFromText} from "./../testHelpers";
+
+describe(nameof(ArrayLiteralExpression), () => {
+    function getArrayLiteralExpression(text: string) {
+        const opts = getInfoFromText<VariableStatement>(text);
+        const declaration = opts.firstChild.getDeclarationList().getDeclarations()[0];
+        return {
+            arrayLiteralExpression: declaration.getFirstChildByKindOrThrow(ts.SyntaxKind.ArrayLiteralExpression) as ArrayLiteralExpression,
+            ...opts
+        };
+    }
+
+    describe(nameof<ArrayLiteralExpression>(e => e.getElements), () => {
+        function doTest(text: string, elementTexts: string[]) {
+            const {arrayLiteralExpression} = getArrayLiteralExpression(text);
+            expect(arrayLiteralExpression.getElements().map(e => e.getText())).to.deep.equal(elementTexts);
+        }
+
+        it("should get the elements when there are none", () => {
+            doTest("var t = []", []);
+        });
+
+        it("should get the elements when there are some", () => {
+            doTest("var t = [5, 3, 'test']", ["5", "3", "'test'"]);
+        });
+    });
+});
