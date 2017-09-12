fb86d80a7dd4e5383b5c829e60b1d6856b97d833
David Sherret <dsherret@gmail.com>
2017-09-11 23:04:56 -0400

    Remove checking diagnostics in getClassViewModels.

--- code-generation/common/getClassViewModels.ts
+++ code-generation/common/getClassViewModels.ts
@@ -3,10 +3,6 @@ import TsSimpleAst, {ClassDeclaration} from "./../../src/main";
 import {getMixinViewModels} from "./getMixinViewModels";
 
 export function* getClassViewModels(ast: TsSimpleAst): IterableIterator<ClassViewModel> {
-    const diagnostics = ast.getDiagnostics().map(m => m.getMessageText());
-    if (diagnostics.length > 0)
-        console.log(diagnostics);
-
     const compilerSourceFiles = ast.getSourceFiles().filter(f => f.getFilePath().indexOf("src/compiler") >= 0);
     const classes = compilerSourceFiles.map(f => f.getClasses()).reduce((a, b) => a.concat(b), []);
 
