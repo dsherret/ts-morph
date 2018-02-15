451840b9f1e1ad9e724369ce5c95a46f74acb63b
David Sherret <dsherret@gmail.com>
2018-02-14 22:22:03 -0500

    feat: Add functions and variables to typescript declaration file.

--- code-generation/common/cloning.ts
+++ code-generation/common/cloning.ts
@@ -23,6 +23,7 @@ export function cloneInterfaces(node: StatementedNode, cloningInterfaces: Interf
             name: method.getName(),
             hasQuestionToken: method.hasQuestionToken(),
             returnType: method.getReturnTypeNodeOrThrow().getText(),
+            docs: method.getJsDocs().map(d => ({ description: d.getInnerText().replace(/\r?\n/g, "\r\n") })),
             typeParameters: method.getTypeParameters().map(p => ({
                 name: p.getName(),
                 constraint: p.getConstraintNode() == null ? undefined : p.getConstraintNode()!.getText()
@@ -71,6 +72,7 @@ export function cloneClasses(node: StatementedNode, classes: ClassDeclaration[])
     node.addClasses(classes.map(c => ({
         name: c.getName(),
         isExported: true,
+        hasDeclareKeyword: true,
         typeParameters: c.getTypeParameters().map(p => ({
             name: p.getName(),
             constraint: p.getConstraintNode() == null ? undefined : p.getConstraintNode()!.getText()
@@ -85,6 +87,7 @@ export function cloneClasses(node: StatementedNode, classes: ClassDeclaration[])
         methods: c.getInstanceMethods().map(method => ({
             name: method.getName(),
             returnType: method.getReturnTypeNodeOrThrow().getText(),
+            docs: method.getJsDocs().map(d => ({ description: d.getInnerText().replace(/\r?\n/g, "\r\n") })),
             typeParameters: method.getTypeParameters().map(p => ({
                 name: p.getName(),
                 constraint: p.getConstraintNode() == null ? undefined : p.getConstraintNode()!.getText()
@@ -101,6 +104,7 @@ export function cloneClasses(node: StatementedNode, classes: ClassDeclaration[])
 export function cloneFunctions(node: StatementedNode, functions: FunctionDeclaration[]) {
     node.addFunctions(functions.map(f => ({
         name: f.getName(),
+        hasDeclareKeyword: true,
         isExported: true,
         typeParameters: f.getTypeParameters().map(p => ({
             name: p.getName(),
@@ -109,26 +113,23 @@ export function cloneFunctions(node: StatementedNode, functions: FunctionDeclara
         docs: f.getJsDocs().map(d => ({ description: d.getInnerText().replace(/\r?\n/g, "\r\n") })),
         parameters: f.getParameters().map(p => ({
             name: p.getNameOrThrow(),
+            hasQuestionToken: p.isOptional(),
             type: p.getTypeNodeOrThrow().getText()
         })),
         returnType: f.getReturnTypeNodeOrThrow().getText()
     })));
 }
 
-/*
 export function cloneVariables(node: StatementedNode, variables: VariableStatement[]) {
-    node.addVariableStatement(variables.map(v => ({
+    node.addVariableStatements(variables.map(v => ({
         isExported: true,
-        typeParameters: f.getTypeParameters().map(p => ({
-            name: p.getName(),
-            constraint: p.getConstraintNode() == null ? undefined : p.getConstraintNode()!.getText()
-        })),
-        docs: f.getJsDocs().map(d => ({ description: d.getInnerText().replace(/\r?\n/g, "\r\n") })),
-        parameters: f.getParameters().map(p => ({
-            name: p.getNameOrThrow(),
-            type: p.getTypeNodeOrThrow().getText()
-        })),
-        returnType: f.getReturnTypeNodeOrThrow().getText()
+        declarationType: v.getDeclarationType(),
+        hasDeclareKeyword: true,
+        docs: v.getJsDocs().map(d => ({ description: d.getInnerText().replace(/\r?\n/g, "\r\n") })),
+        declarations: v.getDeclarations().map(d => ({
+            name: d.getName(),
+            type: d.getTypeNode() == null ? undefined : d.getTypeNodeOrThrow().getText(),
+            initializer: d.getInitializer() == null ? undefined : d.getInitializerOrThrow().getText()
+        }))
     })));
 }
-*/
