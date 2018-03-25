f42c3116878c5c63f3946a2342a9582981d41957
David Sherret <dsherret@gmail.com>
2018-03-24 22:55:05 -0400

    refactor: #243 - Remove "Base" variables from declaration file.
    These won't be exported anymore.

--- scripts/createDeclarationFile.ts
+++ scripts/createDeclarationFile.ts
@@ -5,7 +5,7 @@
  * ----------------------------------------------
  */
 import * as path from "path";
-import Project, {SourceFile, ClassDeclaration, TypeGuards, ts, SyntaxKind} from "../src/main";
+import Project, {SourceFile, ClassDeclaration, TypeGuards, ts, SyntaxKind, VariableStatement} from "../src/main";
 import {StringUtils, ArrayUtils} from "../src/utils";
 import {getDefinitionProject} from "./common";
 import {flattenDeclarationFiles} from "./flattenDeclarationFiles";
@@ -14,5 +14,19 @@ const project = getDefinitionProject();
 const mainFile = project.getSourceFileOrThrow("main.d.ts");
 
 flattenDeclarationFiles(project, mainFile);
+hideBaseDeclarations();
 
 project.save();
+
+function hideBaseDeclarations() {
+    const baseDeclarations = mainFile.getVariableDeclarations().filter(s => StringUtils.endsWith(s.getName(), "Base"));
+
+    for (const declaration of baseDeclarations) {
+        const variableStatement = declaration.getParentOrThrow().getParentOrThrow() as VariableStatement;
+        if (variableStatement.getDeclarations().length > 1)
+            throw new Error(`Unexpected. Found more than one declaration for ${declaration.getName()}.`);
+
+        // the trick is to mark these as not exported in the declaration file
+        variableStatement.setIsExported(false);
+    }
+}
