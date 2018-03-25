477da885ff166a1ee2f24e88ea6c5633caf351b5
David Sherret <dsherret@gmail.com>
2018-03-24 22:53:56 -0400

    chore: Add refactor script to use as a starting point whenever doing programmatic refactors on the library.

---
+++ scripts/refactor.ts
@@ -0,0 +1,20 @@
+﻿/**
+ * Code Manipulation - Refactor
+ * ----------------------------
+ * Use this script as a strating point when refactoring the application.
+ * Edit within the for..of loop.
+ * ----------------------------
+ */
+
+import {InspectorFactory} from "./inspectors";
+
+const factory = new InspectorFactory();
+const tsaInspector = factory.getTsSimpleAstInspector();
+const project = factory.getProject();
+const directory = tsaInspector.getSrcDirectory();
+
+for (const sourceFile of directory.getDescendantSourceFiles()) {
+    /* DON'T CHECK IN THE CHANGES WITHIN THIS BLOCK */
+}
+
+project.save();
