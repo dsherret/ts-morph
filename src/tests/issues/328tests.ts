ee98edd119c518c8eea56f20fcf0c951b96c39e5
David Sherret <dsherret@gmail.com>
2018-05-16 20:39:25 -0400

    chore: Add tests for #328

---
+++ src/tests/issues/328tests.ts
@@ -0,0 +1,23 @@
+﻿import { expect } from "chai";
+import { ClassDeclaration } from "../../compiler";
+import { getInfoFromText } from "../compiler/testHelpers";
+
+describe("tests for issue #328", () => {
+    it("should not add a statement and indent the braces", () => {
+        const text = `
+class MyClass {
+    myMethod() {
+    }
+}`;
+        const { firstChild, sourceFile } = getInfoFromText<ClassDeclaration>(text);
+        firstChild.getMethodOrThrow("myMethod").addStatements("let k = () => { return true; }");
+
+        // cannot reproduce: works fine in this scenario
+        expect(sourceFile.getFullText()).to.equal(`
+class MyClass {
+    myMethod() {
+        let k = () => { return true; }
+    }
+}`);
+    });
+});
