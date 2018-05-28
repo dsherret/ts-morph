c93104cd5f30470b6dd5914d26faaa6153069861
David Sherret <dsherret@gmail.com>
2018-05-27 21:39:02 -0400

    fix: #335 - addStatements would throw if the containing block was only indented a portion of an indentation level.

---
+++ src/tests/issues/335tests.ts
@@ -0,0 +1,20 @@
+﻿import { expect } from "chai";
+import { FunctionDeclaration } from "../../compiler";
+import { getInfoFromText } from "../compiler/testHelpers";
+
+describe("tests for issue #335", () => {
+    it("should be able to add statements at a half indentation level", () => {
+        const text = `
+  function indentedF() {
+  }
+`;
+        const { firstChild, sourceFile } = getInfoFromText<FunctionDeclaration>(text);
+        firstChild.addStatements("return null;");
+
+        expect(sourceFile.getFullText()).to.equal(`
+  function indentedF() {
+      return null;
+  }
+`);
+    });
+});
