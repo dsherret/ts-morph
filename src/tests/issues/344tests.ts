68fa2b1654e97a589ade5a2c70cf23be510831e8
David Sherret <dsherret@gmail.com>
2018-06-09 10:40:29 -0400

    chore: Add tests for #344.

---
+++ src/tests/issues/344tests.ts
@@ -0,0 +1,25 @@
+﻿import { expect } from "chai";
+import { SyntaxKind } from "../../typescript";
+import { getInfoFromText } from "../compiler/testHelpers";
+
+describe("tests for issue #344", () => {
+    it("should add a property assignment with a quoted string", () => {
+        const text = `const t = {};`;
+        const { sourceFile } = getInfoFromText(text);
+        const ole = sourceFile.getVariableDeclarationOrThrow("t")
+            .getInitializerIfKindOrThrow(SyntaxKind.ObjectLiteralExpression);
+
+        ole.addPropertyAssignments([{
+            name: `"test"`,
+            initializer: "5"
+        }, {
+            name: `"with-dash"`,
+            initializer: "5"
+        }, {
+            name: `'single-quotes'`,
+            initializer: "5"
+        }]);
+
+        expect(sourceFile.getFullText()).to.equal(`const t = {\n    "test": 5,\n    "with-dash": 5,\n    'single-quotes': 5\n};`);
+    });
+});
