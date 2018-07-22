c956c6dc688390b147ea07b8dcf2a5e43ff4cb5f
David Sherret <dsherret@gmail.com>
2018-07-22 15:01:53 -0400

    chore: Remove deprecated tests.

--- src/tests/compiler/common/nodeTests.ts
+++ src/tests/compiler/common/nodeTests.ts
@@ -352,30 +352,6 @@ class MyClass {
         });
     });
 
-    describe(nameof<Node>(n => n.getStartColumn), () => {
-        it("should get the column of the node", () => {
-            const {firstChild} = getInfoFromText<ClassDeclaration>("    class MyClass {}");
-            expect(firstChild.getStartColumn()).to.equal(4);
-        });
-
-        it("should get the column of the node including js docs", () => {
-            const {firstChild} = getInfoFromText<ClassDeclaration>("    /** Testing */class MyClass {}");
-            expect(firstChild.getStartColumn(true)).to.equal(4);
-        });
-
-        it("should get the column of the node not including js docs", () => {
-            const {firstChild} = getInfoFromText<ClassDeclaration>("    /** Testing */class MyClass {}");
-            expect(firstChild.getStartColumn()).to.equal(18);
-        });
-    });
-
-    describe(nameof<Node>(n => n.getEndColumn), () => {
-        it("should get the column of the end of the node", () => {
-            const {firstChild} = getInfoFromText<ClassDeclaration>("class MyClass {}");
-            expect(firstChild.getEndColumn()).to.equal(16);
-        });
-    });
-
     describe(nameof<Node>(n => n.getStart), () => {
         function doTest(text: string, expectedPos: number, includeJsDocComment?: boolean) {
             const {firstChild} = getInfoFromText(text);
