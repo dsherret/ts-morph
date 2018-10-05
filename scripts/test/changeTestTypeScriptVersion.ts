eb2897bc49e3cf7c1e03f32bfe875b0eceb644c8
David Sherret <dsherret@gmail.com>
2018-10-04 20:52:07 -0400

    Run through all the tests for every version.

---
+++ scripts/test/changeTestTypeScriptVersion.ts
@@ -0,0 +1,23 @@
+import { Project } from "ts-simple-ast";
+import * as path from "path";
+import { rootFolder } from "../config";
+
+export function changeTestTypeScriptVersion(version: string) {
+    setModuleSpecifierValue(`typescript-${version}`);
+}
+
+export function resetTestTypeScriptVersion() {
+    setModuleSpecifierValue("typescript");
+}
+
+function setModuleSpecifierValue(value: string) {
+    const project = new Project();
+    const sourceFile = project.addExistingSourceFile(path.join(rootFolder, "src/typescript/public.ts"));
+    for (const importDec of sourceFile.getImportDeclarations()) {
+        const moduleSpecifierValue = importDec.getModuleSpecifierValue();
+        if (moduleSpecifierValue.startsWith("typescript"))
+            importDec.setModuleSpecifier(value);
+    }
+
+    sourceFile.saveSync();
+}
--- scripts/test/testTypeScriptVersions.ts
+++ scripts/test/testTypeScriptVersions.ts
@@ -1,15 +1,20 @@
 import { execNpmScript } from "../common";
 import { tsVersionsToTest } from "../config";
+import { changeTestTypeScriptVersion, resetTestTypeScriptVersion } from "./changeTestTypeScriptVersion"
 
 async function run() {
-    // todo: this should pass in which version to use for the tests
     try {
         for (const version of tsVersionsToTest) {
+            console.log(`Changing to version ${version}...`);
+            changeTestTypeScriptVersion(version);
             console.log(`Running tests...`);
             await execNpmScript("test");
         }
+
+        resetTestTypeScriptVersion();
     } catch (err) {
         console.log(err);
+        resetTestTypeScriptVersion();
         process.exit(1);
     }
 }
