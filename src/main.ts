319db5cfdf1cfe35d877834a61c0b017b93f37d5
David Sherret <dsherret@gmail.com>
2018-10-06 14:10:36 -0400

    ts is already exported from public.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 /// <reference path="typings/index.d.ts" />
-import * as ts from "typescript";
 export * from "./codeBlockWriter";
 export * from "./compiler";
 export * from "./errors/classes";
@@ -14,6 +13,4 @@ export { printNode, PrintNodeOptions } from "./utils/compiler/printNode";
 export { SourceFileReferencingNodes } from "./utils/references/SourceFileReferenceContainer";
 export { CompilerOptionsFromTsConfigOptions, CompilerOptionsFromTsConfigResult, getCompilerOptionsFromTsConfig } from "./utils/tsconfig/getCompilerOptionsFromTsConfig";
 export { TypeGuards } from "./utils/TypeGuards";
-// export typescript declarations
 export * from "./typescript/public";
-export { ts };
