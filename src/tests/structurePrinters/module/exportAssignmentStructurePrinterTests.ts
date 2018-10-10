fbf31c0fd69e1e9763c8456f0145f90978c6af64
David Sherret <dsherret@gmail.com>
2018-10-09 20:24:20 -0400

    Export assignment.

---
+++ src/tests/structurePrinters/module/exportAssignmentStructurePrinterTests.ts
@@ -0,0 +1,31 @@
+import { expect } from "chai";
+import { FormatCodeSettings } from "../../../compiler";
+import { ExportAssignmentStructurePrinter } from "../../../structurePrinters";
+import { ExportAssignmentStructure } from "../../../structures";
+import { getStructureFactoryAndWriter } from "../../testHelpers";
+
+describe(nameof(ExportAssignmentStructurePrinter), () => {
+    interface Options {
+        formatCodeSettings?: FormatCodeSettings;
+    }
+
+    function doTest(structure: ExportAssignmentStructure, expectedOutput: string, options: Options = {}) {
+        const { writer, factory } = getStructureFactoryAndWriter(options.formatCodeSettings);
+        factory.forExportAssignment().printText(writer, structure);
+        expect(writer.toString()).to.equal(expectedOutput);
+    }
+
+    // todo: more tests
+
+    describe(nameof<ExportAssignmentStructurePrinter>(p => p.printText), () => {
+        describe("expression", () => {
+            it("should write with a string", () => {
+                doTest({ expression: "testing" }, `export = testing;`);
+            });
+
+            it("should write with a writer and use queued child indentation", () => {
+                doTest({ expression: writer => writer.writeLine("testing |").write("this") }, `export = testing |\n    this;`);
+            });
+        });
+    });
+});
