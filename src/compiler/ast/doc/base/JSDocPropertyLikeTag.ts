98046275a02b9efd4de450b71081ada590204571
David Sherret <dsherret@gmail.com>
2018-10-26 19:50:51 -0400

    feat: Add `JSDocPropertyLikeTag.getName()` and `.getNameNode()`

--- src/compiler/ast/doc/base/JSDocPropertyLikeTag.ts
+++ src/compiler/ast/doc/base/JSDocPropertyLikeTag.ts
@@ -1,14 +1,27 @@
 import { Constructor } from "../../../../types";
+import { ts } from "../../../../typescript";
+import { EntityName } from "../../aliases";
 import { Node } from "../../common";
+import { JSDocTag } from "../JSDocTag";
 
-export type JSDocPropertyLikeTagExtensionType = Node /*& ModifierableNode*/;
+export type JSDocPropertyLikeTagExtensionType = Node<ts.JSDocPropertyLikeTag> & JSDocTag;
 
 export interface JSDocPropertyLikeTag {
-    // todo: methods
+    /** Gets the name of the JS doc property like tag. */
+    getName(): string;
+    /** Gets the name node of the JS doc property like tag. */
+    getNameNode(): EntityName;
 }
 
 export function JSDocPropertyLikeTag<T extends Constructor<JSDocPropertyLikeTagExtensionType>>(Base: T): Constructor<JSDocPropertyLikeTag> & T {
     return class extends Base implements JSDocPropertyLikeTag {
-        // todo: methods
+        // todo: more methods
+        getName() {
+            return this.getNameNode().getText();
+        }
+
+        getNameNode() {
+            return this.getNodeFromCompilerNode(this.compilerNode.name);
+        }
     };
 }
