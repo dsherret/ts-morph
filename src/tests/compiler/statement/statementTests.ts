95ac36a7d5bcb051c585f80e41bdc98799757e36
Sebastian Gurin <sebastigurin@gmail.com>
2018-10-31 22:05:32 -0300

    undo last commit

--- src/tests/compiler/statement/statementTests.ts
+++ src/tests/compiler/statement/statementTests.ts
@@ -1,5 +1,17 @@
+import { expect } from "chai";
 import { Statement } from "../../../compiler";
+import { getInfoFromText } from "../testHelpers";
 
 describe(nameof(Statement), () => {
-    // todo: tests
+
+    describe(nameof<Statement>(n => n.remove), () => {
+        it("should remove the statement from its sourcefile", () => {
+            const { sourceFile, project } = getInfoFromText(`const foo = 1; const bar = 2;`);
+            const statement = sourceFile.getVariableDeclarationOrThrow("foo");
+            statement.remove();
+
+            expect(sourceFile.getText()).to.equals(`const bar = 2;`);
+        });
+    });
+
 });
