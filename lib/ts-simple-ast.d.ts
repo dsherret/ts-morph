801b53daab657400bac03c4ca5952d9b8b04c0da
David Sherret <dsherret@gmail.com>
2018-11-03 17:10:38 -0400

    chore: #485 - DecoratorStructure - Update docs for arguments.

--- lib/ts-simple-ast.d.ts
+++ lib/ts-simple-ast.d.ts
@@ -9997,6 +9997,10 @@ interface SetAccessorDeclarationSpecificStructure {
 
 export interface DecoratorStructure {
     name: string;
+    /**
+     * Arguments for a decorator factory.
+     * @remarks Provide an empty array to make the structure a decorator factory.
+     */
     arguments?: (string | WriterFunction)[] | WriterFunction;
     typeArguments?: string[];
 }
