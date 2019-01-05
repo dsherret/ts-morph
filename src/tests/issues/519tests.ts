c84a2519f0651d14c4daff30611a51ce438470e5
David Sherret <dsherret@gmail.com>
2019-01-05 12:28:25 -0500

    chore: Add tests for #519.

---
+++ src/tests/issues/519tests.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { SyntaxKind } from "../../typescript";
+import { getInfoFromText } from "../compiler/testHelpers";
+
+describe("tests for issue #519", () => {
+    it("should be a tuple for a destructured type", () => {
+        const fileText = `const [ a, ...rest ] = [1, 2]; export { rest };`;
+        const { sourceFile } = getInfoFromText(fileText, { includeLibDts: true });
+
+        const restSymbol = sourceFile.getExportSymbols()[0];
+        const restSymbolType = restSymbol.getTypeAtLocation(restSymbol.getDeclarations()[0]);
+        const restType = sourceFile.getDescendantsOfKind(SyntaxKind.Identifier).find(n => n.getText() === "rest")!.getType();
+
+        expect(restSymbolType.isTuple()).to.be.true;
+        expect(restType.isTuple()).to.be.true;
+    });
+});
