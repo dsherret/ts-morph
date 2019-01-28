b0a1f0ff460fa8820e3e004a1a5f63d431761bd1
David Sherret <dsherret@gmail.com>
2019-01-28 11:33:34 -0500

    chore: Use @types/ts-nameof

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,4 +1,3 @@
-﻿/// <reference path="../../node_modules/ts-nameof/ts-nameof.d.ts" />
-/// <reference path="./customTypings.d.ts" />
+﻿/// <reference path="./customTypings.d.ts" />
 /// <reference path="./globby.d.ts" />
 /// <reference path="./to-absolute-glob.d.ts" />
