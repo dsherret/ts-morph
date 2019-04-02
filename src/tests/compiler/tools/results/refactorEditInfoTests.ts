d45249fae97d3d31711ebf493740d8fda4169ec8
Sebastián Gurín <sgurin@awalabs.com>
2019-04-02 19:07:50 -0300

    test

--- src/tests/compiler/tools/results/refactorEditInfoTests.ts
+++ src/tests/compiler/tools/results/refactorEditInfoTests.ts
@@ -7,11 +7,14 @@ describe(nameof(RefactorEditInfo), () => {
         it("should apply the refactor edits", () => {
             const { sourceFile, project } = getInfoFromText("export function f(a: number, b: string) {}", { filePath: "/f.ts" });
             const languageService = project.getLanguageService();
-            project.createSourceFile("/g.ts", "import {f} from './f'; export const c = f(1, '')");
+            const g = project.createSourceFile("/g.ts", "import {f} from './f'; export const c = f(1, '')");
 
-            const refactorEdits = languageService.getCombinedCodeFix(sourceFile, "fixMissingImport");
+            const edits = languageService.getEditsForRefactor(sourceFile, {}, sourceFile.getFunctionOrThrow("f").getParameterOrThrow("a"),
+            "Convert parameters to destructured object", "Convert parameters to destructured object", {})!;
+            edits.applyChanges();
+            expect(sourceFile.getFullText()).to.equal(``);
+            expect(g.getFullText()).to.equal(``);
 
-            expect(sourceFile.getFullText()).to.equal(`import { Node } from "./Node";\n\nexport class T extends Node {}`);
         });
     });
 });
