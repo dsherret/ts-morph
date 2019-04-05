82b5d1746dd661528066726f06d4bf2eb9450030
Sebastián Gurín <sgurin@awalabs.com>
2019-04-05 14:34:38 -0300

    getTextFromFormattingEdits sort

--- src/manipulation/formatting/getTextFromFormattingEdits.ts
+++ src/manipulation/formatting/getTextFromFormattingEdits.ts
@@ -1,13 +1,17 @@
 ﻿import { SourceFile, TextChange } from "../../compiler";
 
 export function getTextFromFormattingEdits(sourceFile: SourceFile, formattingEdits: ReadonlyArray<TextChange>) {
-    formattingEdits = [...formattingEdits].reverse();
+    // reverse the order
+    formattingEdits = [...formattingEdits].sort((a, b) => {
+        const aStart = a.getSpan().getStart();
+        const bStart = b.getSpan().getStart();
+        const difference = bStart - aStart;
+        return difference === 0 ? -1 : difference < 0 ? -1 : 1;
+    });
     let text = sourceFile.getFullText();
-
     for (const textChange of formattingEdits) {
         const span = textChange.getSpan();
         text = text.slice(0, span.getStart()) + textChange.getNewText() + text.slice(span.getEnd());
     }
-
     return text;
 }
