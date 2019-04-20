c2c13eeb6f8b9d328e1f44e66106b899cc8ff2b3
David Sherret <dsherret@gmail.com>
2019-04-20 18:29:06 -0400

    chore: ClassDeclarationStructurePrinter - Add tests for member order.

--- src/tests/structurePrinters/class/classDeclarationStructurePrinterTests.ts
+++ src/tests/structurePrinters/class/classDeclarationStructurePrinterTests.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { FormatCodeSettings } from "../../../compiler";
+import { FormatCodeSettings, Scope } from "../../../compiler";
 import { ClassDeclarationStructurePrinter } from "../../../structurePrinters";
 import { ClassDeclarationStructure, OptionalKind } from "../../../structures";
 import { getStructureFactoryAndWriter } from "../../testHelpers";
@@ -19,6 +19,64 @@ describe(nameof(ClassDeclarationStructurePrinter), () => {
     // todo: more tests
 
     describe(nameof<ClassDeclarationStructurePrinter>(p => p.printText), () => {
+        describe("member order", () => {
+            it("should write the members in the correct order", () => {
+                const structure: OptionalKind<ClassDeclarationStructure> = {
+                    name: "C",
+                    properties: [{
+                        name: "pNoKeyword"
+                    }, {
+                        scope: Scope.Public,
+                        name: "pPublic"
+                    }, {
+                        scope: Scope.Private,
+                        name: "pPrivate"
+                    }, {
+                        scope: Scope.Protected,
+                        name: "pProtected"
+                    }],
+                    ctors: [{}],
+                    methods: [{
+                        scope: Scope.Private,
+                        name: "m1"
+                    }, {
+                        scope: Scope.Protected,
+                        name: "m2"
+                    }, {
+                        scope: Scope.Public,
+                        name: "m3"
+                    }],
+                    getAccessors: [{ name: "g" }],
+                    setAccessors: [{ name: "s" }]
+                };
+
+                doTest(structure, `class C {
+    pNoKeyword;
+    public pPublic;
+    private pPrivate;
+    protected pProtected;
+
+    constructor() {
+    }
+
+    get g() {
+    }
+
+    set s() {
+    }
+
+    private m1() {
+    }
+
+    protected m2() {
+    }
+
+    public m3() {
+    }
+}`);
+            });
+        });
+
         describe("implements", () => {
             it("should write implements", () => {
                 doTest({ name: "C", implements: ["Base1", writer => writer.write("Base2")] },
