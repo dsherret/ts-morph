3623c510bee70d66b322b8aacb5324188f8383a8
David Sherret <dsherret@gmail.com>
2019-09-29 20:46:34 -0400

    chore: Add failing tests for #708.

---
+++ src/tests/issues/708tests.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { Project } from "../../Project";
+import { IfStatement } from "../../compiler";
+import { TypeGuards } from "../../utils/TypeGuards";
+
+describe("tests for issue #706", () => {
+    it.skip("should not error when removing a else if", () => {
+        const project = new Project({ useVirtualFileSystem: true });
+        const file = project.createSourceFile("test.ts", `if (true) {
+    statement;
+} else if (true) {
+    statement;
+}`);
+        const ifStatement = (file.getStatements()[0] as IfStatement);
+        const elseStatement = ifStatement.getElseStatement()!;
+        elseStatement.remove();
+
+        expect(file.getFullText()).to.equal(`if (true) {
+    statement;
+}`);
+    });
+
+
+    it.skip("should not error", () => {
+        const project = new Project({ useVirtualFileSystem: true });
+        const file = project.createSourceFile("test.ts", `// comment1
+
+if (true) {
+} // comment2
+
+test;`);
+
+        const ifStatement = file.getStatementOrThrow(TypeGuards.isIfStatement);
+        ifStatement.remove();
+    })
+});
