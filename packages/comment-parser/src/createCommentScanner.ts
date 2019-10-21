ce926d3bd447fbbb226af4d91e849d66026f8e24
David Sherret <dsherret@gmail.com>
2019-10-20 20:05:57 -0400

    Probably finish comment scanner.

--- packages/comment-parser/src/createCommentScanner.ts
+++ packages/comment-parser/src/createCommentScanner.ts
@@ -8,93 +8,168 @@ enum CommentKind {
 }
 
 export interface CommentScanner {
+    setParent(parent: ts.Node): void;
+    setFullStart(pos: number): void;
     setPos(newPos: number): void;
-    parseUntilLineBreakOrToken(parent: ts.Node, fullStart: number): Iterable<CompilerCommentNode>;
+    getPos(): number;
+    scanForNewLines(): Iterable<CompilerCommentNode>;
+    scanUntilToken(): Iterable<CompilerCommentNode>;
+    scanUntilNewLineOrToken(): Iterable<CompilerCommentNode>;
 }
 
 export function createCommentScanner(sourceFile: ts.SourceFile): CommentScanner {
     const sourceFileText = sourceFile.text;
+    const commentCache = new Map<number, CompilerCommentNode>();
+    let parent: ts.Node = sourceFile;
+    let fullStart = 0;
     let pos = 0;
 
     return {
+        setParent(newParent: ts.Node) {
+            parent = newParent;
+        },
+        setFullStart(newFullStart: number) {
+            fullStart = newFullStart;
+        },
         setPos(newPos: number) {
             pos = newPos;
         },
-        parseUntilLineBreakOrToken
+        getPos() {
+            return pos;
+        },
+        scanForNewLines,
+        scanUntilToken,
+        scanUntilNewLineOrToken,
     };
 
-    function* parseUntilLineBreakOrToken(parent: ts.Node, fullStart: number) {
+    function* scanForNewLines() {
+        let foundNewLineAfterComment = false;
+        let foundComment = false;
+        while (pos < sourceFileText.length) {
+            const commentKind = skipUntilNewLineTokenOrComment();
+            if (typeof commentKind === "number") {
+                if (foundNewLineAfterComment)
+                    return;
+                yield parseForComment(commentKind);
+                foundComment = true;
+            }
+            else if (commentKind === "non-whitespace") {
+                return;
+            }
+            else if (commentKind === "newline") {
+                if (foundComment)
+                    foundNewLineAfterComment = true;
+            }
+            else {
+                pos++;
+            }
+        }
+    }
+
+    function* scanUntilToken() {
+        while (pos < sourceFileText.length) {
+            const scanResult = skipUntilNewLineTokenOrComment();
+            if (typeof scanResult === "number")
+                yield parseForComment(scanResult);
+            else if (scanResult === "non-whitespace")
+                return;
+        }
+    }
+
+    function* scanUntilNewLineOrToken() {
+        while (pos < sourceFileText.length) {
+            const scanResult = skipUntilNewLineTokenOrComment();
+            if (typeof scanResult === "number")
+                yield parseForComment(scanResult);
+            else
+                return;
+        }
+    }
+
+    function skipUntilNewLineTokenOrComment() {
         while (pos < sourceFileText.length) {
             const commentKind = getCommentKind();
             if (commentKind != null)
-                yield parseForComment(commentKind);
-            else if (!StringUtils.isWhitespace(sourceFileText[pos]))
-                return;
+                return commentKind;
+            else if (!StringUtils.isWhitespaceChar(sourceFileText[pos]))
+                return "non-whitespace";
             else if (sourceFileText[pos] === "\n") {
                 pos++;
-                return;
+                return "newline";
             }
-            else
+            else {
                 pos++;
+            }
         }
+        return undefined;
+    }
 
-        function getCommentKind() {
-            const currentChar = sourceFileText[pos];
-            if (currentChar !== "/")
-                return undefined;
+    function getCommentKind() {
+        const currentChar = sourceFileText[pos];
+        if (currentChar !== "/")
+            return undefined;
 
-            const nextChar = sourceFileText[pos + 1];
-            if (nextChar === "/")
-                return CommentKind.SingleLine;
+        const nextChar = sourceFileText[pos + 1];
+        if (nextChar === "/")
+            return CommentKind.SingleLine;
 
-            if (nextChar !== "*")
-                return undefined;
+        if (nextChar !== "*")
+            return undefined;
 
-            const nextNextChar = sourceFileText[pos + 2];
-            return nextNextChar === "*" ? CommentKind.JsDoc : CommentKind.MultiLine;
-        }
+        const nextNextChar = sourceFileText[pos + 2];
+        return nextNextChar === "*" ? CommentKind.JsDoc : CommentKind.MultiLine;
+    }
 
-        function parseForComment(commentKind: CommentKind) {
+    function parseForComment(commentKind: CommentKind) {
+        const start = pos;
+        let comment = commentCache.get(start);
+        if (comment == null) {
             if (commentKind === CommentKind.SingleLine)
-                return parseSingleLineComment();
-
-            const isJsDoc = commentKind === CommentKind.JsDoc;
-            return parseMultiLineComment(isJsDoc);
-        }
-
-        function parseSingleLineComment() {
-            const start = pos;
-            skipSingleLineComment();
-            const end = pos;
-
-            return new CompilerCommentNode(fullStart, start, end, ts.SyntaxKind.SingleLineCommentTrivia, sourceFile, parent);
-        }
-
-        function skipSingleLineComment() {
-            pos += 2; // skip the slash slash
-
-            while (pos < sourceFileText.length && sourceFileText[pos] !== "\n" && sourceFileText[pos] !== "\r")
-                pos++;
-        }
-
-        function parseMultiLineComment(isJsDoc: boolean) {
-            const start = pos;
-            skipSlashStarComment(isJsDoc);
-            const end = pos;
-
-            return new CompilerCommentNode(fullStart, start, end, ts.SyntaxKind.MultiLineCommentTrivia, sourceFile, parent);
-        }
-
-        function skipSlashStarComment(isJsDoc: boolean) {
-            pos += isJsDoc ? 3 : 2; // skip slash star star or slash star
-
-            while (pos < sourceFileText.length) {
-                if (sourceFileText[pos] === "*" && sourceFileText[pos + 1] === "/") {
-                    pos += 2; // skip star slash
-                    break;
-                }
-                pos++;
+                comment = parseSingleLineComment();
+            else {
+                const isJsDoc = commentKind === CommentKind.JsDoc;
+                comment = parseMultiLineComment(isJsDoc);
             }
+            commentCache.set(start, comment);
+        }
+        else {
+            pos = comment.end;
+        }
+
+        return comment;
+    }
+
+    function parseSingleLineComment() {
+        const start = pos;
+        skipSingleLineComment();
+        const end = pos;
+        return new CompilerCommentNode(fullStart, start, end, ts.SyntaxKind.SingleLineCommentTrivia, sourceFile, parent);
+    }
+
+    function skipSingleLineComment() {
+        pos += 2; // skip the slash slash
+
+        while (pos < sourceFileText.length && sourceFileText[pos] !== "\n" && sourceFileText[pos] !== "\r")
+            pos++;
+    }
+
+    function parseMultiLineComment(isJsDoc: boolean) {
+        const start = pos;
+        skipSlashStarComment(isJsDoc);
+        const end = pos;
+
+        return new CompilerCommentNode(fullStart, start, end, ts.SyntaxKind.MultiLineCommentTrivia, sourceFile, parent);
+    }
+
+    function skipSlashStarComment(isJsDoc: boolean) {
+        pos += isJsDoc ? 3 : 2; // skip slash star star or slash star
+
+        while (pos < sourceFileText.length) {
+            if (sourceFileText[pos] === "*" && sourceFileText[pos + 1] === "/") {
+                pos += 2; // skip star slash
+                break;
+            }
+            pos++;
         }
     }
 }
--- packages/comment-parser/src/tests/createCommentScannerTests.ts
+++ packages/comment-parser/src/tests/createCommentScannerTests.ts
@@ -1,7 +1,95 @@
-import { createCommentScanner } from "../createCommentScanner";
+import { expect } from "chai";
+import { ts } from "@ts-morph/common";
+import { createCommentScanner, CommentScanner } from "../createCommentScanner";
 
 describe(nameof(createCommentScanner), () => {
-    it("should parse the trailing comments", () => {
-
+    it("should cache scanned nodes", () => {
+        const sourceFile = createFile(`// 1`);
+        const scanner = createCommentScanner(sourceFile)
+        const firstScan = Array.from(scanner.scanUntilToken());
+        scanner.setPos(0);
+        const secondScan = Array.from(scanner.scanUntilToken());
+        expect(firstScan).to.deep.equal(secondScan);
     });
+
+    describe(nameof<CommentScanner>(s => s.scanForNewLines), () => {
+        it("should parse the comments on each line", () => {
+            const sourceFile = createFile(`// 1
+/* 2 */ /* 3 */    // 4
+/* 5 */`);
+            const scanner = createCommentScanner(sourceFile);
+            checkScanResult(scanner, ["// 1"]);
+            checkScanResult(scanner, ["/* 2 */", "/* 3 */", "// 4"]);
+            checkScanResult(scanner, ["/* 5 */"]);
+            checkScanResult(scanner, []);
+        });
+
+        it("should parse up to the first token", () => {
+            const sourceFile = createFile(`/* 1 */ test;`);
+            const scanner = createCommentScanner(sourceFile);
+            checkScanResult(scanner, ["/* 1 */"]);
+            checkScanResult(scanner, []);
+        });
+
+        it("should parse after the token", () => {
+            const sourceFile = createFile(`test; // 1\n   /*2*/ //3`);
+            const scanner = createCommentScanner(sourceFile);
+            scanner.setPos(sourceFile.statements[0].end);
+            checkScanResult(scanner, ["// 1"]);
+            checkScanResult(scanner, ["/*2*/", "//3"]);
+            checkScanResult(scanner, []);
+        });
+
+        it("should ignore blank lines", () => {
+            const sourceFile = createFile(`\n  \n // 1\n \n  /*2*/ //3`);
+            const scanner = createCommentScanner(sourceFile);
+            checkScanResult(scanner, ["// 1"]);
+            checkScanResult(scanner, ["/*2*/", "//3"]);
+            checkScanResult(scanner, []);
+        });
+
+        function checkScanResult(scanner: CommentScanner, expectedTexts: string[]) {
+            expect(Array.from(scanner.scanForNewLines()).map(n => n.getText())).to.deep.equal(expectedTexts);
+        }
+    });
+
+    describe(nameof<CommentScanner>(s => s.scanUntilToken), () => {
+        it("should parse until hitting the token", () => {
+            const sourceFile = createFile(`// 1
+/* 2 */ /* 3 */    // 4
+/* 5 */c/*6*/ // 7`);
+            const scanner = createCommentScanner(sourceFile);
+            checkScanResult(scanner, ["// 1", "/* 2 */", "/* 3 */", "// 4", "/* 5 */"]);
+            checkScanResult(scanner, []);
+        });
+
+        function checkScanResult(scanner: CommentScanner, expectedTexts: string[]) {
+            expect(Array.from(scanner.scanUntilToken()).map(n => n.getText())).to.deep.equal(expectedTexts);
+        }
+    });
+
+    describe(nameof<CommentScanner>(s => s.scanUntilNewLineOrToken), () => {
+        it("should parse until hitting a newline", () => {
+            const sourceFile = createFile(`// 1
+/* 2 */ /* 3 */    // 4
+/* 5 */c//5`);
+            const scanner = createCommentScanner(sourceFile);
+            checkScanResult(scanner, ["// 1"]);
+            checkScanResult(scanner, ["/* 2 */", "/* 3 */", "// 4"]);
+            checkScanResult(scanner, ["/* 5 */"]);
+            checkScanResult(scanner, []);
+            expect(scanner.getPos()).to.equal(sourceFile.statements[0].getStart(sourceFile));
+            scanner.setPos(sourceFile.statements[0].end);
+            checkScanResult(scanner, ["//5"]);
+            checkScanResult(scanner, []);
+        });
+
+        function checkScanResult(scanner: CommentScanner, expectedTexts: string[]) {
+            expect(Array.from(scanner.scanUntilNewLineOrToken()).map(n => n.getText())).to.deep.equal(expectedTexts);
+        }
+    });
+
+    function createFile(text: string) {
+        return ts.createSourceFile("./test.ts", text, ts.ScriptTarget.Latest);
+    }
 });
