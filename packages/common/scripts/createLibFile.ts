5281202c20e104a13d3de19b1f5a5a07e5588e2b
David Sherret <dsherret@gmail.com>
2019-11-16 23:59:27 -0500

    chore: #750 - Use dts-minifier to minify the declaration file.
    Strip jsdocs.

--- packages/common/scripts/createLibFile.ts
+++ packages/common/scripts/createLibFile.ts
@@ -1,10 +1,12 @@
 import * as path from "path";
 import * as fs from "fs";
 import fastGlob from "fast-glob";
-import { minifyDeclarationFileText } from "./utils/minifyDeclarationFileText";
+import * as ts from "typescript";
+import { createMinifier } from "dts-minify";
 
 const rootDir = path.resolve(__dirname, "../");
 const libFilesFilePath = path.join(rootDir, "src/data/libFiles.ts");
+const minifier = createMinifier(ts);
 
 fastGlob(`${rootDir.replace(/\\/g, "/")}/node_modules/typescript/lib/lib*.d.ts`).then(filePaths => {
     let libFileText = "// dprint-ignore-file\nexport const libFiles: { fileName: string; text: string; }[] = [";
@@ -16,7 +18,7 @@ fastGlob(`${rootDir.replace(/\\/g, "/")}/node_modules/typescript/lib/lib*.d.ts`)
 
         libFileText += `{\n`
             + `    fileName: "${path.basename(filePath)}",\n`
-            + `    text: \`${minifyDeclarationFileText(fileText).replace(/\r?\n/g, "\\n").replace(/`/g, "\\`")}\`\n`
+            + `    text: \`${minifier.minify(fileText).replace(/\r?\n/g, "\\n").replace(/`/g, "\\`")}\`\n`
             + `}`;
     }
 
