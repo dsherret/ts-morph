378e0c57a8b21641bcb8b0a092702fbf2589583a
David Sherret <dsherret@gmail.com>
2020-04-08 13:09:11 -0400

    chore: Ensure `getTextFromFormattingEdits` sorts the array to prevent a breaking change.

--- packages/ts-morph/src/manipulation/formatting/getTextFromFormattingEdits.ts
+++ packages/ts-morph/src/manipulation/formatting/getTextFromFormattingEdits.ts
@@ -3,20 +3,31 @@ import { SourceFile, TextChange } from "../../compiler";
 
 export function getTextFromTextChanges(sourceFile: SourceFile, textChanges: ReadonlyArray<TextChange | ts.TextChange>) {
     const text = sourceFile.getFullText();
+    const editResult: string[] = []; // it's faster to build up the string in an array
     let start = 0;
-    let end = 0;
-    const editResult: Array<string> = [];
-    for (const textChange of textChanges) {
-        const edit = toWrappedTextChange(textChange);
+
+    for (const { edit } of getSortedTextChanges()) {
         const span = edit.getSpan();
-        end = span.getStart();
-        const beforeEdit = text.slice(start, end);
+        const beforeEdit = text.slice(start, span.getStart());
         start = span.getEnd();
         editResult.push(beforeEdit);
         editResult.push(edit.getNewText());
     }
+
     editResult.push(text.slice(start));
     return editResult.join("");
+
+    function getSortedTextChanges() {
+        // ensure the array is sorted
+        return textChanges.map((edit, index) => ({ edit: toWrappedTextChange(edit), index })).sort((a, b) => {
+            const aStart = a.edit.getSpan().getStart();
+            const bStart = b.edit.getSpan().getStart();
+            const difference = aStart - bStart;
+            if (difference === 0)
+                return a.index < b.index ? -1 : 1;
+            return difference < 0 ? -1 : 1;
+        });
+    }
 
     function toWrappedTextChange(change: TextChange | ts.TextChange) {
         if (change instanceof TextChange)
