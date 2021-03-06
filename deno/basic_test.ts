4b108ededc5339bfca7022759c0533d1a8b9614d
David Sherret <dsherret@gmail.com>
2021-03-06 16:11:17 -0500

    Add very basic deno tests.

---
+++ deno/basic_test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from "https://deno.land/std@0.89.0/testing/asserts.ts";
+import { Project } from "./mod.ts";
+
+// todo: Eventually all tests run for the node package should also be run for Deno
+Deno.test("ts-morph basic tests", () => {
+    const project = new Project({ useInMemoryFileSystemHost: true });
+    const sourceFile = project.createSourceFile("test.ts", "class T {\n}");
+    sourceFile.addClass({
+        name: "Other",
+    });
+    assertEquals(sourceFile.getText(), `class T {\n}\n\nclass Other {\n}\n`);
+});
