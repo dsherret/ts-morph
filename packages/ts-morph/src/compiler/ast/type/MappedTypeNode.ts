d076cf24cbd3143833452c9bb2f2c9ffb203a22e
Nicola Dardanis <nicdard@gmail.com>
2021-04-18 17:50:17 +0200

    feat: Add navigation methods to MappedTypeNode

--- packages/ts-morph/src/compiler/ast/type/MappedTypeNode.ts
+++ packages/ts-morph/src/compiler/ast/type/MappedTypeNode.ts
@@ -4,5 +4,17 @@ import { TypeParameterDeclaration } from "./TypeParameterDeclaration";
 
 export class MappedTypeNode extends TypeNode<ts.MappedTypeNode> {
 
+    /**
+     * Gets the mapped type node's type name if any.
+     */
+    getNameTypeNode(): TypeNode | undefined {
+        return this._getNodeFromCompilerNodeIfExists(this.compilerNode.nameType);
+    }
 
+    /**
+     * Gets the mapped type node's type parameter if any.
+     */
+    getTypeParameter(): TypeParameterDeclaration {
+        return this._getNodeFromCompilerNode(this.compilerNode.typeParameter);
+    }
 }
