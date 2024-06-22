9cd3cf3c75834fb60dfe287ae536ca4cd6dd20bb
David Sherret <dsherret@gmail.com>
2024-06-21 22:23:12 -0400

    Escape

--- packages/bootstrap/scripts/buildDeno.ts
+++ packages/bootstrap/scripts/buildDeno.ts
@@ -50,8 +50,8 @@ import { createProjectSync } from "./mod.ts";
 // todo: Eventually all tests run for the node package should also be run for Deno
 Deno.test("bootstrap general tests", () => {
   const project = createProjectSync({ useInMemoryFileSystem: true });
-  const sourceFile = project.createSourceFile("test.ts", "class T {\n}\n");
-  assertEquals(sourceFile.statements[0].getText(), "class T {\n}");
+  const sourceFile = project.createSourceFile("test.ts", "class T {\\n}\\n");
+  assertEquals(sourceFile.statements[0].getText(), "class T {\\n}");
 });
 `.trimStart(),
 );
--- packages/ts-morph/scripts/buildDeno.ts
+++ packages/ts-morph/scripts/buildDeno.ts
@@ -81,11 +81,11 @@ import { Project } from "./mod.ts";
 // todo: Eventually all tests run for the node package should also be run for Deno
 Deno.test("ts-morph basic tests", () => {
   const project = new Project({ useInMemoryFileSystem: true });
-  const sourceFile = project.createSourceFile("test.ts", "class T {\n}");
+  const sourceFile = project.createSourceFile("test.ts", "class T {\\n}");
   sourceFile.addClass({
     name: "Other",
   });
-  assertEquals(sourceFile.getText(), "class T {\n}\n\nclass Other {\n}\n");
+  assertEquals(sourceFile.getText(), "class T {\\n}\\n\\nclass Other {\\n}\\n");
 });
 `.trimStart(),
 );
