da89454806ae1206cfea2f43bc781487d07ab1dc
David Sherret <dsherret@gmail.com>
2024-06-21 21:52:45 -0400

    Update.

--- packages/bootstrap/scripts/buildDeno.ts
+++ packages/bootstrap/scripts/buildDeno.ts
@@ -1,5 +1,5 @@
 import { tsMorph } from "./deps.ts";
-const { Node, Project } = tsMorph;
+const { Project } = tsMorph;
 
 const project = new Project();
 const fileSystem = project.getFileSystem();
@@ -9,17 +9,3 @@ fileSystem.mkdirSync(destPath);
 fileSystem.copySync("./dist-deno/ts-morph-bootstrap.js", `${destPath}/ts_morph_bootstrap.js`);
 fileSystem.copySync("./lib/ts-morph-bootstrap.d.ts", `${destPath}/ts_morph_bootstrap.d.ts`);
 fileSystem.writeFileSync(`${destPath}/mod.ts`, `// @deno-types="./ts_morph_bootstrap.d.ts"\nexport * from "./ts_morph_bootstrap.js";\n`);
-
-updateCommonImportsExports(project.addSourceFileAtPath(`${destPath}/ts_morph_bootstrap.js`)).saveSync();
-updateCommonImportsExports(project.addSourceFileAtPath(`${destPath}/ts_morph_bootstrap.d.ts`)).saveSync();
-
-function updateCommonImportsExports(file: tsMorph.SourceFile) {
-  for (const statement of file.getStatements()) {
-    if (!Node.isExportDeclaration(statement) && !Node.isImportDeclaration(statement))
-      continue;
-    const moduleSpecifierValue = statement.getModuleSpecifierValue();
-    if (moduleSpecifierValue === "@ts-morph/common")
-      statement.setModuleSpecifier("../common/mod.ts");
-  }
-  return file;
-}
